6a247ac8c68e1dbe5eae036aa33c4faf1dd2846c
ikeyu0806 <ikeyu0806@gmail.com>
2025-11-08 13:49:47 +0900

    Initial Commit

---
+++ ClipStack/ClipStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipStackApp.swift
+//  ClipStack
+//
+//  Created by 池谷祐貴 on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct ClipStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
